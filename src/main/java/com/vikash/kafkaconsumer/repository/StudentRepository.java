373c390019ba12f28bd9c3fa9659f5851d51949d
vikas981 <vs98990@gmail.com>
2022-10-18 07:39:08 +0530

    add query to fetch student by phone number

--- src/main/java/com/vikash/kafkaconsumer/repository/StudentRepository.java
+++ src/main/java/com/vikash/kafkaconsumer/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.vikash.kafkaconsumer.repository;
 import com.vikash.kafkaconsumer.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,5 +12,9 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
 
     @Query("select s from Student s")
     List<Student> getAllStudents();
+
+    @Query("select s from Student s where s.phoneNumber=:phoneNumber")
+    Student getStudentByPhoneNumber(@Param("phoneNumber") String phoneNumber);
+
 
 }
